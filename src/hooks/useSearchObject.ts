1bcf2a73864ce563afc2feb4389e00c2beb804d5
Thompson S <thompson.msds@gmail.com>
2022-09-01 09:44:20 -0300

    hook useSearchObject

---
+++ src/hooks/useSearchObject.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { InterfaceDadosObjeto } from "src/services/apiCorreios/obterDadosRastreioObjeto";
+import { apiCorreios } from "../services/apiCorreios";
+
+export function useSearchObject(codeObject: string) {
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  const [object, setObject] = useState<InterfaceDadosObjeto | null>(null);
+
+  useEffect(() => {
+    apiCorreios.obterDadosRastreioObjeto(codeObject).then(data => {
+      setObject(data);
+    }).catch(error => {
+      setError(error);
+    }).finally(() => {
+      setLoading(false);
+    });
+  }, []);
+
+  return { loading, error, object }
+
+}
